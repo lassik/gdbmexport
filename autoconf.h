0dc6f6da89e06e24a0fb308f10c3b71009a0f35b
Lassi Kortela <lassi@lassi.io>
2022-06-18 23:19:40 +0300

    Add hand-written autoconf.h

---
+++ autoconf.h
@@ -0,0 +1,16 @@
+#define PACKAGE_NAME "gdbmexport-unofficial"
+#define PACKAGE_VERSION "1.14.1"
+#define PACKAGE_BUGREPORT "none@example.com"
+
+#define HAVE_FCNTL_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_FILE_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+
+#define HAVE_BCOPY 1
+#define HAVE_FLOCK 1
+#define HAVE_FSYNC 1
+#define HAVE_FTRUNCATE 1
+#define HAVE_STRUCT_STAT_ST_BLKSIZE 1
